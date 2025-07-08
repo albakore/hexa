8b287c78797e20172c6e44cc5d4bdd4db33af926
albakore <albakoreofficial@gmail.com>
2025-07-08 19:46:53 +0000

    se preparan archivos para los modulos

---
+++ modules/provider/domain/entity/draft_purchase_invoice.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from sqlmodel import SQLModel, Relationship, Field
+
+class DraftPurchaseInvoice(SQLModel, table=True):
+	id : int | None = Field(None, primary_key=True)
+	numero : str = Field(default=None, description="Numero de la factura")
+	concepto : str | None = Field(default=None, description="Concepto")
+	fk_proveedor : int | None = Field(default=None, foreign_key="provider.id")
+	fk_servicio : int | None = Field(default=None, foreign_key="invoiceservice.id")
+	fk_moneda : int | None = Field(default=None, description="EL tipo de moneda")
+
+	awb : str | None = Field(default=None)
+
+	precio_unitario : float | None = Field(default=None)
+	kg : float | None = Field(default=None)
+	items : int | None = Field(default=None)
+
+	fecha_emision : datetime | None = Field(default=None, description="Fecha de emision")
+	fecha_recepcion : datetime | None = Field(default=None, description="Fecha de recepcion")
+
+	fk_invoice : int | None = Field(default=None)
