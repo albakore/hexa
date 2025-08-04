8cdbf51ad90a0d356c21fc595111fc7edb040d15
albakore <albakoreofficial@gmail.com>
2025-08-04 01:50:38 -0300

    react-flow

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,16 @@
+import NodeFlow from "@/components/node_flow/NodeFlow";
 import { ColorModeButton } from "@/components/ui/color-mode";
+import { Box, VStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <div>
-      <ColorModeButton />
-      holas
+    <Box as={'main'}>
+      <NodeFlow/>
+      <ColorModeButton
+        position={'fixed'}
+        top={'10px'}
+        right={'10px'} />
     
-    </div>
+    </Box>
   );
 }
---
+++ frontend/components/node_flow/NodeFlow.tsx
@@ -0,0 +1,120 @@
+'use client'
+import { useState, useCallback, CSSProperties, useId } from 'react';
+import React from 'react';
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, Controls, MiniMap, Background, Node, Position, Handle } from '@xyflow/react';
+import '@xyflow/react/dist/base.css';
+// import './flow.css'
+import { Box, HStack, Text, VStack } from "@chakra-ui/react";
+import { NodeProps } from '@xyflow/react';
+
+const initialNodes : Node[] = [
+	{ id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+	{ id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+	{ id: 'l1', type:'chakra', position: { x: 100, y: 0 }, data: { label: 'Vector Map' } },
+	{ id: 'l2', type:'chakra', position: { x: 100, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges = [
+	{ id: 'n1-n2', source: 'n1', target: 'n2' },
+	// { id: 'l1-l2', source: 'l1-fuo', target: 'l2-fua' },
+	
+];
+
+function MyHandler({ type, name, label }) {
+
+
+	const type_id = type == 'source' ? 'output' : 'input'
+	const direction = type == 'source' ? 'row-reverse' : 'row'
+	const handler_direction = type == 'target' ? Position.Left : Position.Right
+	const css: CSSProperties = {
+		backgroundColor: 'yellow',
+		outline: '1px solid',
+		outlineColor: 'yellow',
+		outlineOffset:'1px',
+		width:'2px',
+		height: '2px',
+		borderRadius: '1000px'
+	}
+	return (
+		<HStack  gap={2} w={'full'} flexDirection={direction} >
+			<Box position={'relative'}>
+				<Handle
+					id={`${name}:${type_id}`}
+					type={type}
+					position={handler_direction}
+					style={css} />
+			</Box>
+			<Text fontSize="xs">{label}</Text>
+		</HStack>
+	)
+}
+
+export function CustomNode(props: NodeProps) {
+
+	const css : CSSProperties = {
+		background: 'red',
+		left : 0
+	}
+
+	return (
+		<Box bg={'blackAlpha.500'} rounded={'md'} border={'xs'} borderColor={'gray.900'} overflow={'hidden'} w={'200px'}>
+			<Box bgGradient="to-r" gradientFrom="blue.600/60" gradientTo="blackAlpha.500" paddingInline={'5px'}>
+				<Text fontSize={'xs'}>{props.data.label}</Text>
+			</Box>
+			<HStack paddingInline={'15px'} w={'full'} paddingBlock={'3px'} alignItems={'start'}>
+				<VStack w={'50%'}>
+					<MyHandler
+						type={'target'}
+						name={'valor'}
+						label={'valor'} />
+					
+				</VStack>
+				<VStack w={'50%'}>
+
+				<MyHandler type={'source'} name={'salida'} label={'salida'}/>
+				<MyHandler type={'source'} name={'salida2'} label={'salida'}/>
+				</VStack>
+			</HStack>
+    </Box>
+  );
+}
+
+const nodeTypes = {
+  chakra: CustomNode,
+};
+
+export default function NodeFlow() {
+	const [nodes, setNodes] = useState(initialNodes);
+	const [edges, setEdges] = useState(initialEdges);
+
+	const onNodesChange = useCallback(
+		(changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+		[],
+	);
+	const onEdgesChange = useCallback(
+		(changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+		[],
+	);
+	const onConnect = useCallback(
+		(params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+		[],
+	);
+
+	return (
+		<div style={{ width: '100vw', height: '100vh' }}>
+
+			<ReactFlow
+				nodes={nodes}
+				edges={edges}
+				nodeTypes={nodeTypes}
+				onNodesChange={onNodesChange}
+				onEdgesChange={onEdgesChange}
+				onConnect={onConnect}
+				fitView
+			>
+				<Controls />
+				<MiniMap />
+				<Background bgColor='rgb(40,40,40)' variant="lines" gap={8} size={1} lineWidth={0.1} />
+			</ReactFlow>
+		</div>
+	);
+}
