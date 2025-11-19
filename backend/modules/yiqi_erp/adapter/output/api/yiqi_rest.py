f843bcc2a3c178410bafc2f34c66caf15b1075ae
albakore <albakoreofficial@gmail.com>
2025-11-19 15:16:32 +0000

    ahora las empresas se consultar si estan activos como proveedor

--- backend/modules/yiqi_erp/adapter/output/api/yiqi_rest.py
+++ backend/modules/yiqi_erp/adapter/output/api/yiqi_rest.py
@@ -230,7 +230,14 @@ class YiqiApiRepository(YiqiRepository):
 		url = "/api/InstancesAPI/GetEntityUpdates2"
 		entity_name = "CLIENTE"
 		last_update = "01011900"
-		aditional_filters = []
+		aditional_filters = [
+			{
+				"columnName": "CLIE_ACTIVO_P",
+				"TipoDato": 2,
+				"operator": 1,
+				"operating": "S",
+			}
+		]
 		attributes = [
 			"CLIE_NOMBRE",
 			"CLIE_RAZON_SOCIAL",
@@ -238,6 +245,8 @@ class YiqiApiRepository(YiqiRepository):
 			"CLIE_MONEDA",
 			"CLIE_REGION",
 			"CLIE_IDIOMA",
+			"CLIE_ACTIVO_P",
+			"CLIE_ACTIVO",
 		]
 		params = {
 			"schemaId": id_schema,
